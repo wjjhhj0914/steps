f38faa42a78071b449df22460f95441be8fc89e1
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-08-20 17:48:34 +0900

    Learn. Don't Set state Manually!
    Use it the way React expects

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const messages = [
 
 export default function App() {
   const [step, setStep] = useState(1);
+  const [test, setTest] = useState({ name: 'Jonas' });
 
   function handlePrevious() {
     if (step > 1) setStep(step - 1);
@@ -15,6 +16,9 @@ export default function App() {
 
   function handleNext() {
     if (step < 3) setStep(step + 1);
+    // BAD PRACTICE 🤦🏻‍♀️
+    // test.name = 'Fred';
+    setTest({ name: 'Fred' });
   }
 
   return (
@@ -26,6 +30,7 @@ export default function App() {
       </div>
       <p className="message">
         Step {step}: {messages[step - 1]}
+        {test.name}
       </p>
       <div className="buttons">
         <button
